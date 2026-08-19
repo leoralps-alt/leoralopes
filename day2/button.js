21f4a83c55b82210645993609d96e25cf7ad8436
Leora Suresh Lopes <leoralps@gmail.com>
2026-08-19 21:00:54 +0530

    initial portfolio

---
+++ day2/button.js
@@ -0,0 +1,12 @@
+const togglebutton = document.getElementById("smash-button")
+togglebutton.addEventListener"click",() => {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")) {
+        themeTitle.innerText = "Dark Mode";
+
+    }
+    else{
+        themeTitle.innerText = "Light Mode";
+    }
+}
